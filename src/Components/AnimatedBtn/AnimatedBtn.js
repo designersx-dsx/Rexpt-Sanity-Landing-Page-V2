df7615684ec020a084e8ffa5c66d22d431c7b3a0
NirajDX <niraj.t@designersx.com>
2025-09-24 17:25:18 +0530

    [AP] Ui new section convertions (AP v1.1.3)

--- src/Components/AnimatedBtn/AnimatedBtn.js
+++ src/Components/AnimatedBtn/AnimatedBtn.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import styles from '../AnimatedBtn/AnimatedBtn.module.css'
 
-const AnimatedBtn = ({ text = "" }) => {
+const AnimatedBtn = ({ 
+    text = "", 
+    text2 = "", 
+    logo = "", 
+    shape = "Svg/shape-btn.svg",  // default shape
+    textColor = "#fff"            // default white text
+}) => {
     return (
-
         <div className={styles.btn}>
-            <img src="Svg/shape-btn.svg" alt="btn-shape" />
-            <p>
-                {text}
-            </p>
+            <img src={shape} alt="btn-shape" className={styles.shape} />
+
+            {text2 && logo ? (
+                <div className={styles.flexContent} style={{ color: textColor }}>
+                    <span>{text2}</span>
+                    <img src={logo} alt="logo" className={styles.logo} />
+                </div>
+            ) : (
+                <p style={{ color: textColor }}>{text}</p>
+            )}
         </div>
     )
 }
